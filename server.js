8f4f8aaaac57962d87af50210f5986057b014c99
Band Ly <bandly@Bands-MacBook-Pro.local>
2017-08-09 14:00:19 -0700

    initial push to github

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var engines = require('consolidate');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.engine('html', engines.mustache);
+app.set("view engine", "html");
+
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+app.listen(3000,function(){
+	console.log("You are now connected!");
+});
+
